3be598f6d9b44f4cb181545d5d6269a75f38a7a0
sgbstats <sebastiangbate@gmail.com>
2025-01-30 16:45:33 +0000

    stuff relating to quarto colour mapping

--- colourimgmaker.R
+++ colourimgmaker.R
@@ -35,3 +35,107 @@ linemaker=function(hex, name)
 #   print(linemaker(partycol[i], parties[i]))
 # }
 
+colour=tribble(~"col", ~"use",
+               "#141414", "Background",
+               "#595959", "Unsure",
+               "#00698F", "Keyword",
+               "#1EDAFB", "Constant",
+               "#58C554", "Numeric",
+               "#58C554", "String",
+               "#997744", "Variable",
+               "#4D4333", "Comment highlight",
+               "#403F3E", "Console error",
+               "#e6e1dc", "Text",
+               "#00AEEF", "function",
+               "#ff308f", "Comment- maybe",
+               "#d44a88", "Better comment")
+
+for(i in 1:nrow(colour))
+{
+  cat(paste0(linemaker(colour$col[i], colour$use[i])),"\n")
+}
+
+
+text_styles <- tribble(
+  ~"use", ~"col",
+    "Normal", "#e6e1dc",
+    "Other", "#FF308F",
+    "Attribute", "#e6e1dc",
+    "SpecialString", "#008000",
+    "Annotation", "#FF308F",
+    "Function", "#FAA61A",
+    "String", "#58C554",
+    "ControlFlow", "#00AEEF",
+    "Operator", "#FFFFFF",
+    "Error", "#403F3E",
+    "BaseN", "#008000",
+    "Alert", "#7928a1",
+    "Variable", "#FFFFFF",
+    "BuiltIn", "null",
+    "Extension", "null",
+    "Preprocessor", "#7928a1",
+    "Information", "#ff308f",
+    "VerbatimString", "#008000",
+    "Warning", "#696969",
+    "Documentation", "#FF308F",
+    "Import", "null",
+    "Char", "#008000",
+    "DataType", "#008000",
+    "Float", "#FAA61A",
+    "Comment", "#d44a88",
+    "CommentVar", "#696969",
+    "Constant", "#FF308F",
+    "SpecialChar", "#00769e",
+    "DecVal", "#58C554",
+    "Keyword", "#FF308F") %>% 
+  arrange(col)
+
+for(i in 1:nrow(text_styles))
+{
+  cat(paste0(linemaker(text_styles$col[i], text_styles$use[i])),"\n")
+}
+
+css_styles <- tribble(
+  ~CSS, ~Property, ~Value,
+  "gutter", "background", "#141414",
+  "gutter", "color", "#595959",
+  "gutter-cellwarning", "background", "#FC0",
+  "print-margin", "background", "#1D1D1D",
+  "marker-layer selection", "background", "#494836",
+  "marker-layer active-line", "background", "#333",
+  "gutter-active-line", "background-color", "#222",
+  "invisible", "color", "#404040",
+  "keyword", "color", "#00698F",
+  "keywordoperator", "color", "#FF308F",
+  "constant", "color", "#1EDAFB",
+  "constantlanguage", "color", "#FDC251",
+  "constantlibrary", "color", "#8DFF0A",
+  "constantnumeric", "color", "#58C554",
+  "invalid", "color", "#FFFFFF",
+  "invalid", "background-color", "#990000",
+  "invaliddeprecated", "color", "#FFFFFF",
+  "invaliddeprecated", "background-color", "#990000",
+  "support", "color", "#999",
+  "supportfunction", "color", "#00AEEF",
+  "function", "color", "#00AEEF",
+  "string", "color", "#58C554",
+  "comment", "color", "#555",
+  "variable", "color", "#997744",
+  "metatag", "color", "#BE53E6",
+  "entityotherattribute-name", "color", "#FFFF89",
+  "fold", "background", "#222",
+  "fold", "color", "#7AF",
+  "marker-layer foreign_line", "background-color", "#2B2B2A",
+  "node-selector", "background-color", "#fb6f1c",
+  "comment-highlight", "color", "#4D4333",
+  "comment-highlight", "background-color", "#D1B78A",
+  "marker-layer active_debug_line", "background-color", "#8B7A27",
+  "marker-layer find_line", "background-color", "#403F3E",
+  "console_error", "background-color", "#403F3E"
+  ) %>%
+  arrange(Value)
+
+
+for(i in 1:nrow(css_styles)){
+  cat(paste0(linemaker(css_styles$CSS[i], css_styles$Property[i])),"\n")
+}
---
+++ quatro.md
@@ -0,0 +1,49 @@
+# Quarto and RStheme colours
+
+
+### this needs fixing
+||RStheme use||
+|---|---|---|
+|![#141414](img/141414.png)| Background|`#141414`|`rgb(20,20,20)`| 
+|![#595959](img/595959.png)| Unsure|`#595959`|`rgb(89,89,89)`| 
+|![#00698F](img/00698F.png)| Keyword|`#00698F`|`rgb(0,105,143)`| 
+|![#1EDAFB](img/1EDAFB.png)| Constant|`#1EDAFB`|`rgb(30,218,251)`| 
+|![#58C554](img/58C554.png)| Numeric|`#58C554`|`rgb(88,197,84)`| 
+|![#58C554](img/58C554.png)| String|`#58C554`|`rgb(88,197,84)`| 
+|![#997744](img/997744.png)| Variable|`#997744`|`rgb(153,119,68)`| 
+|![#4D4333](img/4D4333.png)| Comment highlight|`#4D4333`|`rgb(77,67,51)`| 
+|![#403F3E](img/403F3E.png)| Console error|`#403F3E`|`rgb(64,63,62)`| 
+|![#e6e1dc](img/e6e1dc.png)| Text|`#e6e1dc`|`rgb(230,225,220)`| 
+|![#00AEEF](img/00AEEF.png)| function|`#00AEEF`|`rgb(0,174,239)`| 
+|![#ff308f](img/ff308f.png)| Comment- maybe|`#ff308f`|`rgb(255,48,143)`| 
+|![#d44a88](img/d44a88.png)| Better comment|`#d44a88`|`rgb(212,74,136)`| 
+
+||Quarto use||RStheme|
+|---|---|---|---|
+|![#00769E](img/00769E.png)| SpecialChar|`#ff308f`|`rgb(255,48,143)`| |
+|![#008000](img/008000.png)| SpecialString|`#008000`|`rgb(0,128,0)`| |
+|![#008000](img/008000.png)| BaseN|`#008000`|`rgb(0,128,0)`| |
+|![#008000](img/008000.png)| VerbatimString|`#008000`|`rgb(0,128,0)`| |
+|![#008000](img/008000.png)| Char|`#008000`|`rgb(0,128,0)`| |
+|![#008000](img/008000.png)| DataType|`#008000`|`rgb(0,128,0)`| | 
+  |![#00AEEF](img/A55A00.png)| Function|`#A55A00`|`rgb(165,90,0)`| Functions| 
+|![#00AEEf](img/00AEEf.png)| Constant|`#00AEEf`|`rgb(0,174,239)`| |
+|![#403F3E](img/403F3E.png)| Error|`#403F3E`|`rgb(64,63,62)`| |
+|![#58C554](img/58C554.png)| String|`#58C554`|`rgb(88,197,84)`| String| 
+|![#696969](img/696969.png)| Warning|`#696969`|`rgb(105,105,105)`| |
+|![#696969](img/696969.png)| CommentVar|`#696969`|`rgb(105,105,105)`|| 
+|![#7928A1](img/7928A1.png)| DecVal|`#7928A1`|`rgb(121,40,161)`| Numeric|
+|![#7928a1](img/7928a1.png)| Alert|`#7928a1`|`rgb(121,40,161)`| |
+|![#7928a1](img/7928a1.png)| Preprocessor|`#7928a1`|`rgb(121,40,161)`| |
+|![#A55A00](img/A55A00.png)| Float|`#A55A00`|`rgb(165,90,0)`| |
+|![#D44A88](img/D44A88.png)| Comment|`#D44A88`|`rgb(212,74,136)`|Comment| 
+|![#E6E1DE](img/E6E1DE.png)| Normal|`#E6E1DE`|`rgb(230,225,222)`| |
+|![#E6E1DE](img/E6E1DE.png)| Attribute|`#E6E1DE`|`rgb(230,225,222)`|| 
+|![#FF308F](img/FF308F.png)| Other|`#FF308F`|`rgb(255,48,143)`| |
+|![#FF308F](img/FF308F.png)| Documentation|`#FF308F`|`rgb(255,48,143)`| |
+|![#FF308F](img/FF308F.png)| Keyword|`#FF308F`|`rgb(255,48,143)`| 
+|![#ff308f](img/ff308f.png)| Information|`#FF308F`|`rgb(255,48,143)`| 
+|![#FFFFFF](img/FFFFFF.png)| Annotation|`#FFFFFF`|`rgb(255,255,255)`| 
+|![#FFFFFF](img/FFFFFF.png)| ControlFlow|`#FFFFFF`|`rgb(255,255,255)`| 
+|![#FFFFFF](img/FFFFFF.png)| Operator|`#FFFFFF`|`rgb(255,255,255)`| 
+|![#FFFFFF](img/FFFFFF.png)| Variable|`#FFFFFF`|`rgb(255,255,255)`| 
